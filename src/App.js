bcc278e403b5948487facc60376eac44643964e6
AkruthiJB <78912862+AkruthiJB@users.noreply.github.com>
2022-08-30 15:31:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,11 @@ import './App.css';
 
 function App() {
     return ( < div className = "App" >
-        <
-        header className = "App-header" >
-        <
-        h2 > Good Morning < /h2>
+        <header className = "App-header" >
+        <h2 > Good Morning < /h2>
 
-        <
-        /header >  < /
-        div >
+        </header >  
+            < /div >
     );
 }
 
